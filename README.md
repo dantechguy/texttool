d149b098f75e02b91141fc7df09c32c432ef1624
Daniel Wendon-Blixrud <d@nielwb.com>
2020-08-08 16:38:54 +0100

    updated argument names and readme

--- README.md
+++ README.md
@@ -12,16 +12,16 @@ def wrap_text(
         # the max width of lines. default is shell width
         max_width=shutil.get_terminal_size().columns,
         
-        # number of columns to pad off max_width
+        # number of columns to pad off of max_width
         padding_size=0,
         
         # a string to start all wrapped lines with
-        wrap_prefix='',
+        prefix='',
         
         # a string to end all lines which wrap with
-        wrap_suffix='',
+        suffix='',
         
-        # characters, in priority order, to wrap on
-        wrap_chars=' ,.!',
+        # string of characters, in priority order, to wrap on
+        delimiters=' ,.!',
         ):
 ```
--- textwrapper.py
+++ textwrapper.py
@@ -9,9 +9,9 @@ def wrap_text(
         min_width=1,
         max_width=shutil.get_terminal_size().columns,
         padding_size=0,
-        wrap_prefix='',
-        wrap_suffix='',
-        wrap_characters=' ,.!',
+        prefix='',
+        suffix='',
+        delimiters=' ,.!',
         ):
         
         # TYPE CHECKING
@@ -21,11 +21,11 @@ def wrap_text(
         except TypeError: raise TypeError('max_width must be a number')
         try: padding_size = int(padding_size)
         except TypeError: raise TypeError('padding_size must be a number')
-        try: wrap_prefix = str(wrap_prefix)
-        except TypeError: raise TypeError('wrap_prefix must be a string')
-        try: wrap_suffix = str(wrap_suffix)
-        except TypeError: raise TypeError('wrap_suffix must be a string')
-        try: wrap_characters = str(wrap_characters)
+        try: prefix = str(prefix)
+        except TypeError: raise TypeError('prefix must be a string')
+        try: suffix = str(suffix)
+        except TypeError: raise TypeError('suffix must be a string')
+        try: delimiters = str(delimiters)
         except TypeError: raise TypeError('wrap_chars must be a string')
             
         # VALUE CHECKING
@@ -33,9 +33,9 @@ def wrap_text(
         if padding_size < 0: raise ValueError('padding_size cannot be negative')
         if min_width < 1: raise ValueError('min_width must be at least 1')
         if min_width > max_width: raise ValueError('min_width cannot be greater than line length (max_width-padding_size)')
-        if min_width > max_width - len(wrap_prefix) - len(wrap_suffix) + 1: raise ValueError('min_width cannot be greater than minimum line length (max_width-padding_size-len(wrap_prefix) - len(wrap_suffix))')
+        if min_width > max_width - len(prefix) - len(suffix) + 1: raise ValueError('min_width cannot be greater than minimum line length (max_width-padding_size-len(prefix) - len(suffix))')
         if max_width < 1: raise ValueError('line length (max_width-padding_size) must be at least 1')
-        if len(wrap_prefix) + len(wrap_suffix) >= max_width: raise ValueError('prefix + suffix must be shorter than line length (max_width-padding_size)')
+        if len(prefix) + len(suffix) >= max_width: raise ValueError('prefix + suffix must be shorter than line length (max_width-padding_size)')
         
         result = []
         split_text = text.split('\n')
@@ -43,7 +43,7 @@ def wrap_text(
         for line_text in split_text:
             
             line_result = ''
-            delimiter_indexes = get_delimiter_index_list(line_text, wrap_characters)
+            delimiter_indexes = get_delimiter_index_list(line_text, delimiters)
             line_max_index = len(line_text) - 1
             wrap_index = 0
             
@@ -52,8 +52,8 @@ def wrap_text(
                 wrap_min_index = wrap_index + min_width - 1 # -1 to keep on end of current line
                 wrap_max_index = wrap_index + max_width - 1 # -1 to keep on end of current line
                 if wrap_index > 0:
-                    wrap_min_index -= len(wrap_prefix)
-                    wrap_max_index -= len(wrap_prefix)
+                    wrap_min_index -= len(prefix)
+                    wrap_max_index -= len(prefix)
                 
                 # does rest of line fit in rest of wrap line?
                 if line_max_index <= wrap_max_index:
@@ -65,15 +65,15 @@ def wrap_text(
                     # does rest of line fit in rest of wrap line? NO
                     # text will need to wrap
                     
-                    decreased_wrap_min_index = wrap_min_index - len(wrap_suffix)
-                    decreased_wrap_max_index = wrap_max_index - len(wrap_suffix)
+                    decreased_wrap_min_index = wrap_min_index - len(suffix)
+                    decreased_wrap_max_index = wrap_max_index - len(suffix)
                     
                     # look for greatest viable delimiter index
                     delimiter_index = next(
                         (i for i in delimiter_indexes if (decreased_wrap_min_index <= i <= decreased_wrap_max_index) ),
                         decreased_wrap_max_index ) # if no delimiter found, default to end of line
                         
-                    line_result += line_text[ wrap_index : delimiter_index + 1 ] + wrap_suffix + '\n' + wrap_prefix
+                    line_result += line_text[ wrap_index : delimiter_index + 1 ] + suffix + '\n' + prefix
                     wrap_index = delimiter_index + 1
                     
             result.append(line_result)
@@ -82,17 +82,17 @@ def wrap_text(
 
         
         
-def get_delimiter_index_list(text, wrap_characters):
+def get_delimiter_index_list(text, delimiters):
     delimiter_index_dictionary = {
         character: []
-        for character in wrap_characters }
+        for character in delimiters }
         
     for index, character in enumerate(text):
         if character in delimiter_index_dictionary:
             delimiter_index_dictionary[character].append(index)
             
     delimiter_index_list = []
-    for character in wrap_characters:
+    for character in delimiters:
         delimiter_index_list.extend(delimiter_index_dictionary[character][::-1])
     
     return delimiter_index_list
